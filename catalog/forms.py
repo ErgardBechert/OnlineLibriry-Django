e1e761305e91fbaeb91dc77390f486b5c18f35af
unknown <ergardbehert@gmail.com>
2023-04-10 13:42:13 +0500

    RenewBookForm переписал испльзуя ModelForm

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,22 +1,33 @@
-from django import forms
 
+import datetime
+from django import forms
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
-import datetime #for checking renewal date range.
+from .models import BookInstance
 
-class RenewBookForm(forms.Form):
-    renewal_date = forms.DateField(help_text="Enter a date between now and 4 weeks (default 3).")
-
-    def clean_renewal_date(self):
-        data = self.cleaned_data['renewal_date']
-
-        #Проверка того, что дата не выходит за "нижнюю" границу (не в прошлом).
+class RenewBookForm(forms.ModelForm):
+    def clean_due_back(self):
+        data = self.cleaned_data['due_back']
+        if not data:
+            raise ValidationError(_('Это поле обязательно к заполнению'))
+        try:
+            datetime.datetime.strptime(str(data), '%Y-%m-%d')
+        except ValueError:
+            raise ValidationError(_('Неверный формат даты'))
+        
+        # Проверка того, что дата не в прошлом
         if data < datetime.date.today():
             raise ValidationError(_('Неверная дата - продление в прошлом'))
 
-        #Проверка того, то дата не выходит за "верхнюю" границу (+4 недели).
+        # Проверка, что дата в диапазоне, который разрешен библиотекарю для изменения (сохранения +4 недели)
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
             raise ValidationError(_('Неверная дата - продление более чем на 4 недели вперед'))
 
-        # Помните, что всегда надо возвращать "очищенные" данные.
+        # Не забывайте всегда возвращать очищенные данные
         return data
+
+    class Meta:
+        model = BookInstance
+        fields = ['due_back',]
+        labels = { 'due_back': _('Renewal date'), }
+        help_texts = { 'due_back': _('Enter a date between now and 4 weeks (default 3).'), }
--- catalog/views.py
+++ catalog/views.py
@@ -79,35 +79,6 @@ import datetime
 
 from .forms import RenewBookForm
 
-@permission_required('catalog.can_mark_returned')
-def renew_book_librarian(request, pk):
-    """
-    View function for renewing a specific BookInstance by librarian
-    """
-    book_inst = get_object_or_404(BookInstance, pk=pk)
-
-    # If this is a POST request then process the Form data
-    if request.method == 'POST':
-
-        # Create a form instance and populate it with data from the request (binding):
-        form = RenewBookForm(request.POST)
-
-        # Check if the form is valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_inst.due_back = form.cleaned_data['renewal_date']
-            book_inst.save()
-
-            # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
-
-    # If this is a GET (or any other method) create the default form.
-    else:
-        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
-
-    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
-
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -128,11 +99,11 @@ def renew_book_librarian(request, pk):
         if form.is_valid():
             # Обработка данных из form.cleaned_data
             #(здесь мы просто присваиваем их полю due_back)
-            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.due_back = form.cleaned_data['due_back']
             book_inst.save()
 
             # Переход по адресу 'all-borrowed':
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('profile') )
 
     # Если это GET (или какой-либо ещё), создать форму по умолчанию.
     else:
